f16e55770efe29a6ff2e2eb4e750ea1a2a2e459b
Louis-haywood <louishaywood5@gmail.com>
2026-03-18 18:59:02 +0000

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_HOST', '127.0.0.1:3306');
+define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'u463907152_HRTech');
 define('DB_USER', 'u463907152_HRTech');
 define('DB_PASS', 'Harry2026!');
